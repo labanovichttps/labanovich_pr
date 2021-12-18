db7e697eab060b6f286c028396eb4730afa3a6b2
loverloser <timofeilobanovich@gmail.com>
2021-12-18 08:42:40 +0300

    all completed

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,26 +1,35 @@
 create table technic
 (
-    id           int auto_increment primary key,
-    name         varchar(128) not null,
-    producer     varchar(128) not null,
-    produce_date date         not null,
-    service_date date         not null,
-    cost         double       not null
+    id           int auto_increment
+        primary key,
+    name         varchar(128)              not null,
+    producer     varchar(128)              not null,
+    produce_date date                      not null,
+    service_date date                      not null,
+    cost         double                    not null,
+    isSurrender  varchar(20) default 'Нет' not null
 );
 
 create table employee
 (
-    id           int auto_increment primary key,
+    id           int auto_increment
+        primary key,
     name         varchar(128) not null,
     surname      varchar(128) not null,
     position     varchar(128) not null,
-    phone_number varchar(128)
+    phone_number varchar(128) null
 );
 
 create table surrender_technic
 (
-    id             int primary key auto_increment,
-    user_id        int references employee (id),
-    technic_id     int references technic (id),
-    surrender_date date default null
+    id             int auto_increment
+        primary key,
+    user_id        int  null,
+    technic_id     int  null,
+    surrender_date date not null,
+    receive_date   date null,
+    constraint surrender_technic_employee_id_fk
+        foreign key (user_id) references employee (id),
+    constraint surrender_technic_technic_id_fk
+        foreign key (technic_id) references technic (id)
 );
